be74d8d428ac31faf7c6c5094fcc1c584722b273
Jinesh Jain <jain.jinesh@outlook.com>
2022-01-12 20:01:42 +0530

    Modified with instructions.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
 import download_file
 import regex_for_key
 
+print('''
+    \nWelcome to the Google Drive Downloader.\n
+    This script is developed on python. it takes url and asks file name for input
+    and downlods the file associated with the url link.
+    -> If you are executing this on windows then enter the filename with file format
+    -> If you are on linux, just the file name is fine.
+    \n\n**NOTE: The url link must have permission that anyone with this link can view the file 
+    _______________________________________________________________________________________________
+''')
+
 fileUrl = input("Enter the url of the file:")
 fileName = input("\nEnter the name of the file (with extension) to be save as:")
 
